4f76fd813d2291e7e78d4c3d68749954d542dbc5
Max Sayer <sayer_max@yahoo.co.uk>
2020-06-23 16:39:52 +0100

    Simplified

--- README.md
+++ README.md
@@ -1,37 +1,9 @@
 # MC-Backup
-A spigot plugin which periodically, and on command, emails a back-up of a Minecraft server.
-
-Created with both Java and Python.
-
-## Setup
-#### Details on SMTP
-An email account for sending and receiving the backups will need to be made. Details regarding using gmail for this can be found here: https://www.androidauthority.com/gmail-smtp-settings-801100/
-#### Python Script
-- Place `mc_backup.py` into a directory of choice along with a `config.json` file.
-- Fill in config.json with the appropriate configurations. <br> ```javascript
-{
-    "server_path" : "",
-    "backup_folder_path" : "",
-    "ignored_directories" : [],
-    "ignored_files" : [],
-    "sender_email" : "",
-    "sender_password" : "",
-    "receiver_emails" : [],
-    "email_title" : "Minecraft SMP",
-    "smtp_server" : "",
-    "smtp_port" : 0
-}```
-- Many email providers put limits onto attachment capacity, to avoid maximising the space available, directories such as `plugins` and files such as the `server.jar` can be ignored in backups.
-#### Spigot Plugin
-- Place `backup.jar` into the server's plugins folder.
-- Go into the `config.yml` file and fill in the configurations.<br>`frequency: 72000
-path: path/script.py
-python: python3
-debug: false`
-- `frequency` is the interval in ticks in which backups are made.
-- `path` is the path to the python script.
-- `python` is the python command, dependent on os.
-- `debug` shows errors and output of the python script.
+A python script which checks if a Minecraft server is online with a playercount greater than 0. If so it creates a backup `.zip` of the server.
 
 ## Usage
-Backups are sent to the `receiver_emails` in the `config.json` file, and stored in the `backup_folder_path` folder. In game, operators can issue the command `/backup` to backup the server. If they wish to send the backup to another email, or multiple, they can issue the command with arguments `/backup a@gmail.com b@gmail.com`.
+- `python3 mc_backup.py {absolute_server_path} {absolute_backup_folder_path} {server_address}`
+- Requires the backup folder to be made.
+
+## Requirements
+See `requirements.txt`.
--- mc_backup.py
+++ mc_backup.py
@@ -1,117 +1,68 @@
 # https://github.com/sayersauce
-# Usage, execute using command `python3 mc_backup.py {extra_recipient} {extra_recipient}...`
+# Usage `python3 mc_backup.py {absolute_server_path} {absolute_backup_folder_path} {server_address}`
 
-import smtplib
-import ssl
-import json
 import os
 import sys
+from mcstatus import MinecraftServer
 from datetime import datetime
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.multipart import MIMEMultipart
 from zipfile import ZipFile
 
 
-FILE_PATH = os.path.dirname(os.path.realpath(__file__)) + "/"
-
-
-def load_config():
-    # Loads configuration from config.json.
-    with open(FILE_PATH + "config.json") as config_file:
-        return json.load(config_file)
-
-
 def timestamp(zip = False):
-    # Returns a timestamp, format changes due to filename restrictions.
     date = datetime.now()
     if zip:
         return f"{date.day}-{date.month}-{date.year} {date.hour}-{date.minute}-{date.second}"
     return f"{date.day}/{date.month}/{date.year} - {date.hour}:{date.minute}:{date.second}"
 
 
-def create_backup_zip(server_path, backup_folder_path, ignored_dirs, ignored_files):
-    # Zips all server files. Saves the zip to `Backups` folder. Returns the zip's path.
+def create_backup_zip(server_path, backup_folder_path):
     file_paths = []
 
-    # Get all file paths from server.
+    # Get all file paths from server
     for root, dirs, files in os.walk(server_path):
-        skip = False
-
-        for d in ignored_dirs:
-            if d in root:
-                skip = True
-                break
-
-        if skip:
-            continue
-
         for f in files:
-            if f not in ignored_files:
-                file_paths.append(os.path.join(root, f))
+            file_paths.append(os.path.join(root, f))
 
-    path = f"{backup_folder_path}{timestamp(True)}.zip"
+    zip_path = f"{backup_folder_path}{timestamp(True)}.zip"
 
-    # Zipping.
-    with ZipFile(path, "w") as zip: 
+    # Zipping
+    with ZipFile(zip_path, "w") as zip: 
         for file in file_paths: 
             zip.write(file)
 
-    return path
+    return zip_path
 
 
-def send_email(sender, password, recipient, zip_path, subject_title, smtp_server, port):
-    # Sends zipfile to recipient email.
-    message = MIMEMultipart()
-    message["From"] = sender
-    message["To"] = recipient
-    message["Subject"] = subject_title + " " + timestamp()
-
-    # Attaching zip.
-    with open(zip_path, "rb") as attachment:
-        part = MIMEBase("application", "octet-stream")
-        part.set_payload(attachment.read())
-
-    encoders.encode_base64(part)
-    part.add_header(
-        "Content-Disposition",
-        "attachment; filename= backup.zip"
-    )
-    message.attach(part)
-    
-    # Sending email.
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender, password)
-        server.sendmail(sender, recipient, message.as_string())
+def player_count(address):
+    server = MinecraftServer.lookup(address)
+    try:
+        status = server.status()
+        return status.players.online
+    except:
+        print("Error pinging Minecraft Server.")
+        return 0
 
 
 if __name__ == "__main__":
-    # Check args.
+    # Check args
     args = sys.argv
-    recipients = []
 
-    if len(args) > 1:
-        for arg in args[1:]:
-            recipients.append(arg)
+    if len(args) > 3:
+        server_path = args[1]
+        backup_folder_path = args[2]
+        server_address = args[3]
 
-    # Config.
-    config = load_config()
+        if not server_path.endswith("/"):
+            server_path += "/"
+        if not backup_folder_path.endswith("/"):
+            backup_folder_path += "/"
 
-    # Perform backup creation.
-    zip_path = create_backup_zip(FILE_PATH + config["server_path"], FILE_PATH + config["backup_folder_path"], config["ignored_directories"], config["ignored_files"])
-
-    # Send email.
-    recipients += config["receiver_emails"]
-
-    for recipient in recipients:
-        print(f"Sending to {recipient}")
-        send_email(
-            config["sender_email"],
-            config["sender_password"],
-            recipient,
-            zip_path,
-            config["email_title"],
-            config["smtp_server"],
-            config["smtp_port"]
-        )
+        # Check that the server has players online
+        if player_count(server_address):
+            # Perform backup creation
+            zip_path = create_backup_zip(server_path, backup_folder_path)
+            print("Zip created at " + zip_path + ".")
+        else:
+            print("No zip created.")
+    else:
+        print("Usage: mc_backup.py {server_path} {backup_folder_path} {server_address}")
